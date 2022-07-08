76308b9fd2726254cddaac612cd94631fe41bfc6
Jose Nortes <nooors@gmail.com>
2022-07-08 11:14:25 +0200

    forms running without conexion with backend

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormGroup,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styles: [],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = this.fb.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(8)]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  processForm() {
+    console.log(this.loginForm.value);
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
   }
-
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styles: [
-  ]
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styles: [],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = this.fb.group({
+    name: ["", [Validators.required, Validators.minLength(2)]],
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(8)]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  processRegister() {
+    console.log(this.registerForm.value);
   }
-
 }
